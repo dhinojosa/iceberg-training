12c0a998b284182696297ba9f3ce81d773d243ad
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2025-09-10 13:04:17 -0600

    type(docker-compose): FIX: Fixed docker and s3a connectivity
    Fixed all docker for spark and working very well!

--- projects/images/pyspark-notebook/create-local-image.sh
+++ projects/images/pyspark-notebook/create-local-image.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t dhinojosa/pyspark-iceberg-notebook:0.1 -f Dockerfile .
+docker build -t dhinojosa/pyspark-iceberg-notebook:0.1.1 -f Dockerfile .
--- projects/images/pyspark-notebook/create-multiarch-image.sh
+++ projects/images/pyspark-notebook/create-multiarch-image.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker buildx create --use && docker buildx build --platform linux/amd64,linux/arm64 -t dhinojosa/pyspark-iceberg-notebook:0.1 --push -f Dockerfile .
+docker buildx create --use && docker buildx build --platform linux/amd64,linux/arm64 -t dhinojosa/pyspark-iceberg-notebook:0.1.1 --push -f Dockerfile .
